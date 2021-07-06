d930f08c817d438b5599b42c521c2dec02f3a6ef
Marko Tomčík <markotomcik@gmail.com>
2021-07-06 13:39:48 +0200

    Link Component

--- src/LinkComponent.js
+++ src/LinkComponent.js
@@ -1,16 +1,14 @@
 import React from 'react'
 
-function Link() {
-  let id, desc;
-
+function Link({ person }) {
   return (
     <>
       <div className="bg-white w-80 text-center mx-16 my-20 py-4 bg-clip-padding rounded-2xl backdrop-filter backdrop-blur-xl bg-opacity-25 border border-gray-200">
-        <img src={id} alt={desc} />
-        <p>Marek Kormoš:</p>
-        <p>Chalan zo Svitu</p>
+        <img src={person.id} alt={person.desc} />
+        <p>{person.name}:</p>
+        <p>{person.desc}</p>
         <button className="border rounded-lg px-20 mt-4">
-          <a href="/en/kormos">Show CV</a>
+          <a href={'/en/' + person.url}>Show CV</a>
         </button>
       </div>
     </>
--- src/MainComponent.js
+++ src/MainComponent.js
@@ -2,15 +2,17 @@ import React from 'react'
 import Link from './LinkComponent'
 
 function Main() {
-  /* var json = require('./CVs.json')
-  const Person = json['en'][parseInt(id['id']) - 1]
-  console.log(Person)
-  var Meno = Person['name']
-  var Age = Person['age'] */
+  var json = require('./lang/CV/en.json')
+  console.log(json)
 
   return (
     <div className="flex flex-wrap h-full justify-center items-stretch bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500">
-      <Link />
+      {
+        json.map((person) => {
+          console.log(person)
+          return <Link person={person} />
+        })
+      }
     </div>
   )
 }

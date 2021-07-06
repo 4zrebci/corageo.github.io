385a3eb4d201a9f9470aaf28b171e03dfae2e6be
Pluvas159 <maniksamuel159@gmail.com>
2021-07-06 12:29:11 +0200

    better routing and added SK language to CVComponent

--- src/App.js
+++ src/App.js
@@ -6,48 +6,47 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  Link,
+  Redirect
 } from "react-router-dom";
 
 function App() {
   return (
     <Router>
+      <Header />
   <Switch>
-  <Route path="">
-       <div>
-        <Header />
-        <Main />
-        <Footer />
-       </div>
-    </Route>
+  <Route exact path="/">
+  <Main />
+  </Route>
     <Route path="/en/home">
-       <div>
-        <Header />
-        <Main />
-        <Footer />
-       </div>
+      <Redirect to="/" />
     </Route>
-    <Route path="/en/kormoš">
-        <Header />
-        <CV id="2" />
-        <Footer />
+    <Route path="/en/kormos">
+        <CV id="2" lang = 'en' />
     </Route>
-    <Route path="/en/švagerko">
-        <Header />
-        <CV id="3" />
-        <Footer />
+    <Route path="/en/svagerko">
+        <CV id="3" lang = 'en' />
     </Route>
-    <Route path="/en/tomčík">
-        <Header />
-        <CV id="4" />
-        <Footer />
+    <Route path="/en/tomcik">
+        <CV id="4" lang = 'en' />
     </Route>
     <Route path="/en/manik">
-        <Header />
-        <CV id="1" />
-        <Footer />
+        <CV id="1" lang = 'en' />
+    </Route>
+    <Route path="/sk/kormos">
+        <CV id="2" lang = "sk" />
+    </Route>
+    <Route path="/sk/tomcik">
+        <CV id="4" lang = "sk" />
+    </Route>
+    <Route path="/sk/manik">
+        <CV id="1" lang = "sk" />
+    </Route>
+    <Route path="/sk/svagerko">
+        <CV id="3" lang = "sk" />
     </Route>
   </Switch>
+  <Footer />
     </Router>
 
   )

6451912da8ef57b8e2e44c20d6f9631401564b66
LonziSlovakia <lonzikormos@gmail.com>
2021-07-07 12:09:43 +0200

    Vymenil som SK a EN za vlajky

--- client/src/HeaderComponent.js
+++ client/src/HeaderComponent.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import corageoLogo from './img/corageoLogo.png'
+import sksvg from './img/001-slovakia.svg'
+import ensvg from './img/002-united-kingdom.svg'
 import DropdownButton from 'react-bootstrap/DropdownButton'
 import Dropdown from 'react-bootstrap/Dropdown'
 import {useLocation}  from 'react-router-dom/'
@@ -13,9 +15,13 @@ function Header() {
             <li className="font-display text-yellow-600 text-xl ml-80 pt-8">
               <DropdownButton drop="right" title="Lang" className=" border rounded-lg p-2">
                 <div className="ml-4 border rounded-lg py-2 -mt-2">
-                  <Dropdown.Item className="rounded-lg px-2 py-2 hover:bg-gray-300" href={`/sk/${useLocation().pathname.slice(4)}`}>sk</Dropdown.Item>
-                  <p className="inline"> / </p> 
-                  <Dropdown.Item className="rounded-lg px-2 py-2 hover:bg-gray-300" href={`/en/${useLocation().pathname.slice(4)}`}>en</Dropdown.Item>
+                  <Dropdown.Item className="rounded-lg px-2 py-2 hover:bg-gray-300" href={`/sk/${useLocation().pathname.slice(4)}`}>
+                    <img className="inline-block h-8 w-8" src={sksvg} />
+                  </Dropdown.Item>
+                  <p className="inline"> / </p>
+                  <Dropdown.Item className="rounded-lg px-2 py-2 hover:bg-gray-300" href={`/en/${useLocation().pathname.slice(4)}`}>
+                    <img className="inline-block h-8 w-8" src={ensvg} />
+                  </Dropdown.Item>
                 </div>
               </DropdownButton>
             </li>

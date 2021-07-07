95180ba8119b47bf686976642dd8a99851fed7ba
LonziSlovakia <lonzikormos@gmail.com>
2021-07-07 12:14:54 +0200

    Upravil som vlajky a zoradenie elementov v headeri

--- client/src/HeaderComponent.js
+++ client/src/HeaderComponent.js
@@ -10,9 +10,9 @@ function Header() {
   return (
     <>
         <nav>
-          <ul className="bg-white flex content-around">
+          <ul className="bg-white flex justify-between content-around mr-40">
             <a href="/"><li className="px-20"><img src={corageoLogo} alt="CORAgeo Logo" /></li></a>
-            <li className="font-display text-yellow-600 text-xl ml-80 pt-8">
+            <li className="font-display text-yellow-600 text-xl pt-8 self-start">
               <DropdownButton drop="right" title="Lang" className=" border rounded-lg p-2">
                 <div className="ml-4 border rounded-lg py-2 -mt-2">
                   <Dropdown.Item className="rounded-lg px-2 py-2 hover:bg-gray-300" href={`/sk/${useLocation().pathname.slice(4)}`}>
